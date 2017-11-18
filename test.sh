acb993659852c10da9d479c563826f2f4a4acd96
Torstein Vik <torsteinv64@gmail.com>
2017-11-19 00:43:35 +0100

    Added coloring to test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+NC='\033[0m'
+
 process() {
-     cat
+    GREP_COLOR='01;31' egrep --color=always '.*\[failed\]|$' | GREP_COLOR='01;32' egrep --color=always '.*\[succeeded\]|$'
 }
 
 gp -q ./src/main/* ./src/test/test.gp ./src/test/tests.gp | process
 OUT=${PIPESTATUS[0]}
 if [ $OUT -eq 0 ]; then
-    echo "All tests passed!"
+    echo -e "${GREEN}All tests passed!${NC}"
 else
-    echo "Some tests failed!"
+    echo -e "${RED}Some tests failed!${NC}"
 fi
 exit $OUT
